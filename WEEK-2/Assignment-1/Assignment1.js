6bde051d8bee9f39f8940d6ba0f4efaf88e40b89
Scott Fan <tea79649@hotmail.com>
2023-01-17 23:58:48 +0800

    v2

--- WEEK-2/Assignment-1/Assignment1.js
+++ WEEK-2/Assignment-1/Assignment1.js
@@ -1,11 +1,10 @@
 function max(numbers){
   // your code here, for-loop method preferred
-  let large = 0;  
-  //[1.2]
+  let large = number[0];  
   for ( let i = 0; i < numbers.length; i++ ) {
-      if (numbers[i] > large) {    //number[i] = 2, large = 2
-       large = numbers[i]          
-     	}
+    if (numbers[i] > large) {    
+     large = numbers[i]          
+    }
   }
   return large;
 }
@@ -24,10 +23,6 @@ console.log(max([5, 2, 7, 1, 6])); // should print 7
 
 console.log(findPosition([5, 2, 7, 1, 6], 5)); // should print 0
 // // 表示要尋找在array[5, 2, 7, 1, 6]中,5的位置。
- console.log(max([1, 2, 4, 5])); // should print 5
- console.log(max([5, 2, 7, 1, 6])); // should print 7
-
- console.log(findPosition([5, 2, 7, 1, 6], 5)); // should print 0
  console.log(findPosition([5, 2, 7, 1, 6], 7)); // should print 2
  console.log(findPosition([5, 2, 7, 7, 7, 1, 6], 7)); 
  // should print2 (the first position)
