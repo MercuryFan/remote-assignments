2ecc522281323e1ed51102b8987b70d2eb1453d6
辰星她爹 <mercury@chenxingtadiedeMacBook-Pro.local>
2023-02-05 01:00:53 +0800

    ok

--- Week-4/Assignment-2/script.js
+++ Week-4/Assignment-2/script.js
@@ -1,19 +1,19 @@
 function ajax(src, callback) {
     // your code here
        fetch(src).then(callback)
-   }
-   function render(data) {
+}
+function render(data) {
     // your code here
     // document.createElement() and appendChild() methods arepreferred.
-        return data.json().then(function(data){
-            let result = document.querySelector('#result');
-            result.innerHTML = '';
-            for(let i = 0; i < data.length; i++) {
-                let product= data[i];
-                result.innerHTML += `<div> 產品名稱：${product.name} , 產品售價：${product.price} , 產品描述：${product.description}</div>`
-            }
-        })
-    }
+    return data.json().then(function(data){
+        let result = document.querySelector('#result');
+        result.innerHTML = '';
+        for(let i = 0; i < data.length; i++) {
+            let product= data[i];
+            result.innerHTML += `<div> 產品名稱：${product.name} , 產品售價：${product.price} , 產品描述：${product.description}</div>`
+        }
+    })
+}
 
 
 
